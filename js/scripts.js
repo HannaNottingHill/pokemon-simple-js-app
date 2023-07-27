c7da7be971ea274ca01870781100ccb0f1d9663b
HannaNottingHill <hanacajkas@icloud.com>
2023-07-27 13:31:51 +0100

    New Pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
-alert ();
-var favoriteFood = 'gelato';
-document.write (favoriteFood);
+let pokemonList = [{name: "Vulpix",height: "2'00\" ", type:"Fire" },
+{name: "Pikachu",height: "1'04\" ", type:"Electric" },
+{name: "Snorlax",height: "6'11\" ", type:"Normal" },
+{name: "Rapidash",height: "5'07\" ", type:"Fire" },
+{name: "Onix",height: "28'10\" ", type :["Rock", "Ground"]},
+{name: "Seadra",height: "3'11\" ", type:"Water" },
+{name: "Charizard",height: "5'07\" ", type:["Fire", "Flying"]},
+{name: "Balbasaur",height: "2'04\" ", type:["Gras", "Poison"]}
+]
