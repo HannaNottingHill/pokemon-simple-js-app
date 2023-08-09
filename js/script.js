4c2c2a05734e897b1366b2da5428b80516a20c30
HannaNottingHill <hanacajkas@icloud.com>
2023-08-09 07:47:43 +0100

    uncomment

--- js/script.js
+++ js/script.js
@@ -59,11 +59,11 @@ function add(item) {
  //Event listener  for each pokemon, showDetails
 
 
- //function showDetails (pokemon) {
+ function showDetails (pokemon) {
     console.log (pokemon.name);
    }
 
-//pokemonRepository.getAll().forEach ((pokemon) => {
+pokemonRepository.getAll().forEach ((pokemon) => {
    pokemonRepository.addListItem(pokemon);
     });
 
