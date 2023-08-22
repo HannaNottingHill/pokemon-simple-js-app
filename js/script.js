4271da620f6d7ab9fa1bec379d34839600a580d3
HannaNottingHill <hanacajkas@icloud.com>
2023-08-22 23:21:11 +0100

    bootstrap modal added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let pokemonRespiratory = (function () {
+  let pokemonRepository = (function () {
     let pokemonList = []; //list to store fetched pokemon
     let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20";
 
     // addListItem function
     function addListItem(pokemon) {
-      let pokemonListElement = document.querySelector(".pokemon-list");
+      let pokemonListElement = document.querySelector(".list-group");
 
-      let listpokemon = document.createElement("li");
+      let listPokemon = document.createElement("a");
+      listPokemon.classList.add("list-group-element");
       let button = document.createElement("button");
-
-      button.classList.add("pokemon-button");
+      button.classList.add("btn", "btn-primary");
       button.innerText = pokemon.name;
 
-      listpokemon.appendChild(button);
-      pokemonListElement.appendChild(listpokemon);
+      listPokemon.appendChild(button);
+      pokemonListElement.appendChild(listPokemon);
 
       // event listener for the buttons
       button.addEventListener("click", function (event) {
@@ -90,59 +90,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // function to show modal
   function showModal(pokemonName, pokemonHeight, pokemonImage) {
-    //Modal added
-    let modalContainer = document.querySelector("#modal-container");
-    modalContainer.classList.add("is-visible");
+    let modal = document.querySelector("#pokemonModal");
+    let modalTitle = modal.querySelector(".modal-title");
+    let modalImage = modal.querySelector("#pokemonImage");
+    let modalHeight = modal.querySelector("#pokemonHeight");
 
-    modalContainer.innerHTML = "";
-    let modal = document.createElement("div");
-    modal.id = "modal";
-    modal.classList.add("modal");
+    modalTitle.innerText = pokemonName;
+    modalImage.src = pokemonImage;
+    modalImage.alt = pokemonName;
+    modalHeight.innerText = pokemonHeight;
 
-    //close button element
-    let closeButtonElement = document.createElement("button");
-    closeButtonElement.classList.add("modal-close");
-    closeButtonElement.innerText = "close";
-
-    closeButtonElement.addEventListener("click", hideModal);
-
-    // title element
-    let titleElement = document.createElement("h1");
-    titleElement.innerText = pokemonName;
-
-    //p element
-    let contentElement = document.createElement("p");
-    contentElement.innerText = pokemonHeight;
-
-    //Pokemon image!!
-
-    let imgconatiner = document.createElement("div");
-    imgconatiner.classList.add("img-container");
-    let pokemonImg = document.createElement("img");
-    //pokemonImage.src = item.url;
-
-    pokemonImg.src = pokemonImage;
-    pokemonImg.alt = pokemonName;
-
-    //pokemonImg = pokemonHeight;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-    modal.appendChild(imgconatiner);
-    imgconatiner.appendChild(pokemonImg);
-
-    modal.classList.add("is-visible");
+    $(modal).modal("show");
   }
 
-  // hide modal function
+  //hide modal function
   function hideModal() {
-    let modal = document.querySelector("#modal-container");
+    let modal = document.querySelector("#modal");
     modal.classList.remove("is-visible");
   }
 
-  // event listener to close modal when clicking 'esc'
+  //event listener to close modal when clicking 'esc'
   window.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       hideModal();
@@ -150,13 +117,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Load pokemon list and details, then render list items
-  pokemonRespiratory.loadList().then(function () {
-    let allPokemon = pokemonRespiratory.getAll();
+  pokemonRepository.loadList().then(function () {
+    let allPokemon = pokemonRepository.getAll();
 
-    Promise.all(allPokemon.map(pokemonRespiratory.loadDetails)).then(
+    Promise.all(allPokemon.map(pokemonRepository.loadDetails)).then(
       function () {
         allPokemon.forEach(function (pokemon) {
-          pokemonRespiratory.addListItem(pokemon);
+          pokemonRepository.addListItem(pokemon);
         });
       }
     );
